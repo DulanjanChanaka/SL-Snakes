0cbf3a13825dad3c0a0a6ba173b6bb332749eb71
dulanjanchanaka@gmail.com <dulanjanchanaka@gmail.com>
2023-04-29 14:07:00 +0530

    update 4 files

--- src/Pages/Category/Category.jsx
+++ src/Pages/Category/Category.jsx
@@ -8,7 +8,7 @@ const Category = () => {
        <div ><PoisonSliders title=" උග්‍ර විෂ " rawsCount={1} slidesPerView={1} poisonType="1"/></div> 
        <div ><PoisonSliders title=" මද විෂ " rawsCount={1} slidesPerView={1} poisonType="2"/></div> 
        <div ><PoisonSliders title=" සුළු විෂ  " rawsCount={1} slidesPerView={1} poisonType="3"/></div> 
-       <div ><PoisonSliders title=" නිර් විෂ  " rawsCount={1} slidesPerView={1} poisonType="4"/></div> 
+       <div><PoisonSliders  title=" නිර් විෂ  " rawsCount={1} slidesPerView={1} poisonType="4"/></div> 
       </div>
   )
 }
